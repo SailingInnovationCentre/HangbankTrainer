dfca4e3066b55924d5daaf06c2e4627e6bcebdc2
bartcramer <bart.cramer@gmail.com>
2019-10-09 13:57:57 +0200

    Introduction of HangbankConfiguration object.

---
+++ HangbankTrainer/HangbankTrainer/HangbankTrainerConfiguration.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HangbankTrainer
+{
+    class HangbankTrainerConfiguration : INotifyPropertyChanged
+    {
+
+        private double _linksOnbelast;
+        public double LinksOnbelast
+        {
+            get => _linksOnbelast;
+            set => SetField(ref _linksOnbelast, value);
+        }
+
+        private double _linksBelast;
+        public double LinksBelast
+        {
+            get => _linksBelast;
+            set => SetField(ref _linksBelast, value);
+        }
+
+        private double _rechtsOnbelast;
+        public double RechtsOnbelast
+        {
+            get => _rechtsOnbelast;
+            set => SetField(ref _rechtsOnbelast, value);
+        }
+
+        private double _rechtsBelast;
+        public double RechtsBelast
+        {
+            get => _rechtsBelast;
+            set => SetField(ref _rechtsBelast, value);
+        }
+
+        private double _spelerMinimum;
+        public double SpelerMinimum
+        {
+            get => _spelerMinimum;
+            set => SetField(ref _spelerMinimum, value);
+        }
+
+        private double _spelerMaximum;
+        public double SpelerMaximum
+        {
+            get => _spelerMaximum;
+            set => SetField(ref _spelerMaximum, value);
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true; 
+        }
+
+        #endregion
+    }
+}
--- HangbankTrainer/HangbankTrainer/MainWindow.xaml.cs
+++ HangbankTrainer/HangbankTrainer/MainWindow.xaml.cs
@@ -50,33 +50,34 @@ namespace HangbankTrainer
         {
             InitializeComponent();
 
-            SeriesCollection = new SeriesCollection
-            {
-                new LineSeries
-                {
-                    Title = "Links",
-                    Values = new ChartValues<int>(Enumerable.Range(1, 100).Select(x => 0))
-                },
-                new LineSeries
-                {
-                    Title = "Rechts",
-                    Values = new ChartValues<int>(Enumerable.Range(1, 100).Select(x => 0))
-                }
+            //SeriesCollection = new SeriesCollection
+            //{
+            //    new LineSeries
+            //    {
+            //        Title = "Links",
+            //        Values = new ChartValues<int>(Enumerable.Range(1, 100).Select(x => 0))
+            //    },
+            //    new LineSeries
+            //    {
+            //        Title = "Rechts",
+            //        Values = new ChartValues<int>(Enumerable.Range(1, 100).Select(x => 0))
+            //    }
 
-            };
+            //};
 
-            _listener = new SerialPortListener("COM4");
-            _listener.NewMessage += (s, e) =>
-            {
-                var eventArgs = (SerialPortEventArgs)e; 
-                SeriesCollection[0].Values.Add(eventArgs.Left);
-                SeriesCollection[0].Values.RemoveAt(0); 
-                SeriesCollection[1].Values.Add(eventArgs.Right);
-                SeriesCollection[1].Values.RemoveAt(0);
-            };
-            _listener.Open();
+            //_listener = new SerialPortListener("COM4");
+            //_listener.NewMessage += (s, e) =>
+            //{
+            //    var eventArgs = (SerialPortEventArgs)e; 
+            //    SeriesCollection[0].Values.Add(eventArgs.Left);
+            //    SeriesCollection[0].Values.RemoveAt(0); 
+            //    SeriesCollection[1].Values.Add(eventArgs.Right);
+            //    SeriesCollection[1].Values.RemoveAt(0);
+            //};
+            //_listener.Open();
 
-            DataContext = this;
+            DataContext = new HangbankTrainerConfiguration();
+            ConfigurationUserControl.DataContext = DataContext; 
 
         }
 
@@ -89,7 +90,11 @@ namespace HangbankTrainer
 
         private void Window_Closed(object sender, EventArgs e)
         {
-            _listener.Close(); 
+            //_listener.Close(); 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
