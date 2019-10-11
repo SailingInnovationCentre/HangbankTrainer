d0836e2407f216365112e500a579c9c3227c42f6
bartcramer <bart.cramer@gmail.com>
2019-10-11 14:57:03 +0200

    Add dummy data generator for quicker development.

--- HangbankTrainer/HangbankTrainer/SerialPortListener.cs
+++ HangbankTrainer/HangbankTrainer/SerialPortListener.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Timers;
 
 namespace HangbankTrainer
 {
@@ -11,13 +12,17 @@ namespace HangbankTrainer
 
         public event EventHandler NewMessage;
 
+        private Timer _timer;
+        private Random _random = new Random(); 
+
         internal void SetSerialPort(string port)
         {
             try
             {
                 if (_serialPort != null)
                 {
-                    _serialPort.Open();
+                    _serialPort.Close();
+                    _serialPort = null;
                 }
             }
             catch (Exception)
@@ -25,23 +30,36 @@ namespace HangbankTrainer
                 // gulp.
             }
 
-            _serialPort = new SerialPort(port);
-
-            _serialPort.BaudRate = 9600;
-            _serialPort.Parity = Parity.None;
-            _serialPort.StopBits = StopBits.One;
-            _serialPort.DataBits = 8;
-            _serialPort.Handshake = Handshake.None;
-
-            _serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
-
-            try
+            if (_timer != null)
             {
-                _serialPort.Open(); 
+                _timer.Stop();
+                _timer = null; 
             }
-            catch (Exception)
+
+            if (port == "test")
             {
-                // gulp.
+                StartTestLoop(); 
+            }
+            else
+            {
+                _serialPort = new SerialPort(port);
+
+                _serialPort.BaudRate = 9600;
+                _serialPort.Parity = Parity.None;
+                _serialPort.StopBits = StopBits.One;
+                _serialPort.DataBits = 8;
+                _serialPort.Handshake = Handshake.None;
+
+                _serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+                try
+                {
+                    _serialPort.Open();
+                }
+                catch (Exception)
+                {
+                    // gulp.
+                }
             }
         }
 
@@ -58,6 +76,18 @@ namespace HangbankTrainer
                 _cachedString = _cachedString.Substring(indexNewline+1); 
                 indexNewline = _cachedString.IndexOf('\n'); 
             }
+        }
+
+        private void StartTestLoop()
+        {
+            _timer = new Timer();
+            _timer.AutoReset = true;
+            _timer.Interval = 500;
+            _timer.Elapsed += (s, e) =>
+            {
+                NewMessage?.Invoke(this, new SerialPortEventArgs(0, _random.Next(245, 255)));
+            };
+            _timer.Start(); 
         }
 
     }
