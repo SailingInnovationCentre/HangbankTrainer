053f59a2f1c143b6e4dde29bd940f6662afd7748
bartcramer <bart.cramer@gmail.com>
2019-10-16 15:25:43 +0200

    First attempt at adding player profiles

---
+++ HangbankTrainer/HangbankTrainer/Athlete.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HangbankTrainer
+{
+    class Athlete
+    {
+
+        public string Name { get; set; }
+
+        public int LengthCm { get; set; }
+
+        public int WeightKg { get; set; }
+
+        public int MomentMax { get; set; }
+
+        public int Moment75 { get; set; }
+
+        public int Moment145degrees { get; set; }
+
+    }
+}
---
+++ HangbankTrainer/HangbankTrainer/AthletePersister.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HangbankTrainer
+{
+    static class AthletePersister
+    {
+
+        public static void Write(string path, IEnumerable<Athlete> athletes)
+        {
+            var sb = new StringBuilder(); 
+            foreach (var athlete in athletes)
+            {
+                sb.Append(String.Join(',', new[] {
+                    athlete.Name,
+                    athlete.LengthCm.ToString(),
+                    athlete.WeightKg.ToString(),
+                    athlete.MomentMax.ToString(),
+                    athlete.Moment75.ToString(),
+                    athlete.Moment145degrees.ToString(),
+                }));
+            }
+            File.WriteAllText(path, sb.ToString()); 
+        }
+
+        public static IEnumerable<Athlete> Read(string path)
+        {
+            var lines = File.ReadAllLines(path);
+            foreach (var line in lines)
+            {
+                var elements = line.Split(',');
+                yield return new Athlete
+                {
+                    Name = elements[0],
+                    LengthCm = int.Parse(elements[1]),
+                    WeightKg = int.Parse(elements[2]),
+                    MomentMax = int.Parse(elements[3]),
+                    Moment75 = int.Parse(elements[4]),
+                    Moment145degrees = int.Parse(elements[5])
+                };
+            }
+        }
+
+    }
+}
