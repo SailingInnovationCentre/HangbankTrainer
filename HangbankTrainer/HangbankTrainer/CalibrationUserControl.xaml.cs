c1b49eb5704f7f7d9c5f711edfbd6f55f6bb195b
bartcramer <bart.cramer@gmail.com>
2019-10-22 22:59:15 +0200

    Calibration with timers.

--- HangbankTrainer/HangbankTrainer/CalibrationUserControl.xaml.cs
+++ HangbankTrainer/HangbankTrainer/CalibrationUserControl.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -42,13 +43,123 @@ namespace HangbankTrainer
 
         private void OnMessage(object sender, EventArgs e)
         {
-            var eventArgs = (SerialPortEventArgs)e;
-
-            var linksMoment = (int)(1000 * (eventArgs.Left - _model.LinksOnbelast) / (_model.LinksBelast - _model.LinksOnbelast));
-
-            Value = linksMoment; 
+            Value = CalculateMoment(e); 
         }
 
+        private int _secondsLeft; 
+        public int SecondsLeft
+        {
+            get => _secondsLeft;
+            set => SetField(ref _secondsLeft, value);
+        }
+
+        private int CalculateMoment(EventArgs e)
+        {
+            var serialPortEventArgs = (SerialPortEventArgs)e;
+            return (int)(1000 * (serialPortEventArgs.Left - _model.LinksOnbelast) / (_model.LinksBelast - _model.LinksOnbelast));
+        }
+
+        private void StopCalibrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            _model.Listener.NewMessage -= OnMessage; 
+            _mainWindow.StartFrontPage(); 
+        }
+
+        private void OnMessageDuringCalibration(object sender, EventArgs e)
+        {
+            // TODO: Differentiate left/right. 
+            var linksMoment = CalculateMoment(e); 
+
+            _sumOfMoments += linksMoment;
+            _numberOfMoments++; 
+        }
+
+        long _sumOfMoments;
+        int _numberOfMoments;
+        Timer _timer; 
+
+        enum CalibrationValue
+        {
+            Min, Game, Max
+        };
+
+        private CalibrationValue _calibratedValue; 
+
+        private void StartCalibration()
+        {
+            LockButtons(true); 
+
+            _sumOfMoments = 0;
+            _numberOfMoments = 0;
+            SecondsLeft = 10;
+
+            _model.Listener.NewMessage += OnMessageDuringCalibration;
+
+            _timer = new Timer();
+            _timer.AutoReset = true;
+            _timer.Interval = 500;
+            _timer.Elapsed += CalibrationTimerElapsed;
+            _timer.Start(); 
+        }
+
+        private void CalibrationTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            SecondsLeft--;
+            if (SecondsLeft == 0)
+            {
+                _timer.Stop();
+
+                // Prevent event leaks.
+                _timer.Elapsed -= CalibrationTimerElapsed;
+                _model.Listener.NewMessage -= OnMessageDuringCalibration;
+
+                // Calculate outcome of calibration.
+                int avgMoment = (int)(_sumOfMoments / (double)_numberOfMoments);
+                if (_calibratedValue == CalibrationValue.Min)
+                {
+                    _model.CurrentAthlete.MomentMin = avgMoment;
+                }
+                if (_calibratedValue == CalibrationValue.Game)
+                {
+                    _model.CurrentAthlete.MomentGame = avgMoment;
+                }
+                else if (_calibratedValue == CalibrationValue.Max)
+                {
+                    _model.CurrentAthlete.MomentMax = avgMoment;
+                }
+
+                LockButtons(false);
+            }
+        }
+
+        private void CalibrateMinButton_Click(object sender, RoutedEventArgs e)
+        {
+            _calibratedValue = CalibrationValue.Min;
+            StartCalibration(); 
+        }
+        
+        private void CalibrateGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            _calibratedValue = CalibrationValue.Game;
+            StartCalibration();
+        }
+        
+        private void CalibrateMaxButton_Click(object sender, RoutedEventArgs e)
+        {
+            _calibratedValue = CalibrationValue.Max;
+            StartCalibration();
+        }
+
+        private void LockButtons(bool shouldLock)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                CalibrateMinButton.IsEnabled = !shouldLock;
+                CalibrateGameButton.IsEnabled = !shouldLock;
+                CalibrateMaxButton.IsEnabled = !shouldLock;
+                StopCalibrationButton.IsEnabled = !shouldLock;
+            });
+        }
 
         #region INotifyPropertyChanged
 
@@ -66,26 +177,5 @@ namespace HangbankTrainer
         }
 
         #endregion
-
-        private void StopCalibrationButton_Click(object sender, RoutedEventArgs e)
-        {
-            _model.Listener.NewMessage -= OnMessage;
-            _mainWindow.StartFrontPage(); 
-        }
-        
-        private void CalibrateMinButton_Click(object sender, RoutedEventArgs e)
-        {
-            _model.CurrentAthlete.MomentMin = _value;  
-        }
-        
-        private void CalibrateGameButton_Click(object sender, RoutedEventArgs e)
-        {
-            _model.CurrentAthlete.MomentGame = _value;
-        }
-        
-        private void CalibrateMaxButton_Click(object sender, RoutedEventArgs e)
-        {
-            _model.CurrentAthlete.MomentMax = _value;
-        }
     }
 }
